f7998d755cdb442f4a8ce85e159934b9811777e9
yihdego <yihdego@msn.com>
2018-01-16 17:37:30 -0800

    official answer

--- cake4.rb
+++ cake4.rb
@@ -1,11 +1,5 @@
 sample = [[0, 1], [3, 5], [4, 8], [10, 12], [9, 10]]
 
-def merge_ranges(input)
-
-end
-
-merge_ranges(sample)
-
 two_ranges = [[1,3],[2,4]]
 
 def one_pair(input)
@@ -29,3 +23,30 @@ def one_pair(input)
 end
 
 one_pair(two_ranges)
+
+def merge_ranges(meetings)
+
+# sort by start time
+sorted_meetings = meetings.sort
+
+# initialize merged_meetings with the earliest meeting
+merged_meetings = [sorted_meetings[0]]
+
+sorted_meetings[1..-1].each do |current_meeting_start, current_meeting_end|
+
+  last_merged_meeting_start, last_merged_meeting_end = merged_meetings[-1]
+
+  # if the current and last meetings overlap, use the latest end time
+  if current_meeting_start <= last_merged_meeting_end
+    merged_meetings[-1] = [last_merged_meeting_start, [last_merged_meeting_end, current_meeting_end].max]
+
+  # add the current meeting since it doesn't overlap
+  else
+    merged_meetings.push([current_meeting_start, current_meeting_end])
+  end
+end
+
+p merged_meetings
+end
+
+merge_ranges(sample)
